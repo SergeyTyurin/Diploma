468a810b1f44993849a9f84917779f92a708f2bb
VladislavTyurin <vst123@yandex.ru>
2017-11-25 14:45:05 +0300

    Добавлена возможность перезаписи .bash_profile

--- configure.py
+++ configure.py
@@ -1,13 +1,26 @@
 import os
 
+def writeEnvParam(bash_data,envName,envParam):
+    lines = bash_data
+    index=0
+    for line in lines:
+        if line.find(envName)!=-1:
+            lines[index] = "export {0}={1}\n".format(envName,envParam)
+            return lines
+        index+=1
+    lines.append("export {0}={1}\n".format(envName,envParam))
+    return lines
+
 try:
-    bashfile = open("{0}/.bash_profile".format(os.getenv("HOME")),'a+')
+    bashfile = open("{0}/.bash_profile".format(os.getenv("HOME")),'r+')
     envDiplomaDIR = os.getcwd()
     envLMDBDataSetDIR = os.path.abspath(os.path.join(envDiplomaDIR,"..","DataSet","lmdb"))
     envTestDataSetDIR = os.path.abspath(os.path.join(envDiplomaDIR, "..", "DataSet", "Destination","Test"))
-
-    bashfile.write("export DiplomaDIR={0}\n".format(envDiplomaDIR))
-    bashfile.write("export LMDBDataSetDIR={0}\n".format(envLMDBDataSetDIR))
-    bashfile.write("export TestDataSetDIR={0}\n".format(envTestDataSetDIR))
+    bash_data = bashfile.readlines()
+    bash_data = writeEnvParam(bash_data,"DiplomaDIR",envDiplomaDIR)
+    bash_data = writeEnvParam(bash_data,"LMDBDataSetDIR",envLMDBDataSetDIR)
+    bash_data = writeEnvParam(bash_data,"TestDataSetDIR",envTestDataSetDIR)
+    bashfile.seek(0)
+    bashfile.writelines(bash_data)
 except Exception as e:
     print(e)
