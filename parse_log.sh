acc0fe45c05ceb9b7ef46fad87cce0c68401686d
VladislavTyurin <vst123@yandex.ru>
2017-12-03 16:46:22 +0300

    Изменения в парсере лога

--- parse_log.sh
+++ parse_log.sh
@@ -1,48 +1,51 @@
 #!/bin/bash
 
-if [[ $# == 0 || $# == 1 ]]; then
-    echo "Требуется 2 армумента 1 - директория с логом, 2 - имя файла"
+if [[ $# == 0 || $# == 1 || $# == 2  ]]; then
+    echo "Требуется 3 армумента: 1 - модель, 2 - директория с логом, 3 - имя файла"
     exit 0
 else
-	if ! [[ -d $1 ]]; then
-		echo "Директория $1 не найдена"
+    if ! [[ -d $1 ]]; then
+		echo "Модель $1 не найдена"
+		exit 0
+	elif ! [[ -d $1/$2 ]]; then
+		echo "Директория $1/$2 не найдена"
 		exit 0
 	else
-		if ! [[ -f $1/$2 ]]; then
-			echo "Файл $1/$2 не найден"
+		if ! [[ -f $1/$2/$3 ]]; then
+			echo "Файл $1/$2/$3 не найден"
 			exit 0
 		fi
 	fi
 fi
 
-if ! [[ -d $1/data ]]; then
-	mkdir $1/data
+if ! [[ -d $1/$2/data ]]; then
+	mkdir $1/$2/data
 else
-	echo $1/data
+	echo $1/$2/data
 fi
 
-if ! [[ -d $1/data/train ]]; then
-	mkdir $1/data/train
+if ! [[ -d $1/$2/data/train ]]; then
+	mkdir $1/$2/data/train
 else
-	echo $1/data/train
+	echo $1/$2/data/train
 fi
 
-if ! [[ -d $1/data/val ]]; then
-	mkdir $1/data/val
+if ! [[ -d $1/$2/data/val ]]; then
+	mkdir $1/$2/data/val
 else
-	echo $1/data/val
+	echo $1/$2/data/val
 fi
 
-TRAIN="$1/data/train"
-VAL="$1/data/val"
+TRAIN="$1/$2/data/train"
+VAL="$1/$2/data/val"
 
-grep "^.*Iteration.*loss =.*$" $1/$2 | grep -o "Iteration [0-9]*" | grep -o [0-9][0-9]* > $TRAIN/Train_iterations.txt
-grep "^.*Iteration.*loss =.*$" $1/$2 | grep -o "loss = [0-9]*\.[0-9]*.*" | grep -o "[0-9]*\.[0-9]*.*" > $TRAIN/Train_loss.txt
-grep "Test" $1/$2 | grep -o "Iteration \d*" | grep -o "\d\d*" > $VAL/Val_iterations.txt
-grep "Test" $1/$2 | grep -o "loss = [0-9]\.[0-9]*.*" | grep -o "[0-9]\.[0-9]*.*" > $VAL/Val_loss.txt
-grep "Test" $1/$2 | grep -o "accuracy = [0-9]\.[0-9]*.*" | grep -o "[0-9]\.[0-9]*.*" > $VAL/Val_acc.txt
+grep "^.*Iteration.*loss =.*$" $1/$2/$3 | grep -o "Iteration [0-9]*" | grep -o [0-9][0-9]* > $TRAIN/Train_iterations.txt
+grep "^.*Iteration.*loss =.*$" $1/$2/$3 | grep -o "loss = [0-9]*\.[0-9]*.*" | grep -o "[0-9]*\.[0-9]*.*" > $TRAIN/Train_loss.txt
+grep "Test" $1/$2/$3 | grep -o "Iteration \d*" | grep -o "\d\d*" > $VAL/Val_iterations.txt
+grep "Test" $1/$2/$3 | grep -o "loss = [0-9]\.[0-9]*.*" | grep -o "[0-9]\.[0-9]*.*" > $VAL/Val_loss.txt
+grep "Test" $1/$2/$3 | grep -o "accuracy = [0-9]\.[0-9]*.*" | grep -o "[0-9]\.[0-9]*.*" > $VAL/Val_acc.txt
 
-~/myproject/venv/bin/python Create_CSV.py --train_csv_file $TRAIN/train.csv\
+~/Diploma/env/bin/python Create_CSV.py --train_csv_file $TRAIN/train.csv\
                                        --val_csv_file $VAL/val.csv\
                                        --train_iterations_file $TRAIN/Train_iterations.txt\
                                        --train_loss_file $TRAIN/Train_loss.txt\
